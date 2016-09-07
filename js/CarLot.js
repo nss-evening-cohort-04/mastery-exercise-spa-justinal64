ec9d4fabfc19a752449e6a9d43d9b9b52430e610
justin leggett <justin@codingrookie.com>
2016-09-07 16:25:46 -0500

    createCard not fully working

--- js/CarLot.js
+++ js/CarLot.js
@@ -3,11 +3,33 @@ var CarLot = (function () {
 
   return {
     loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
-
-      inventoryLoader.addEventListener("load", function () {
-
+        var loader = new XMLHttpRequest();
+        // loader.addEventListener("load", onLoad);
+        loader.addEventListener("error", function() {
+            console.log("There was an error!");
+        });
+        loader.open("GET", "json/inventory.json")
+        loader.send();
+        loader.addEventListener("load", function () {
+        // Set the value of the private array
+        inventory = JSON.parse(this.responseText);
+        // Invoke the callback function
+        callback(inventory); // callback = populatePage
       });
+    }, getInventory: function() {
+        console.log(inventory);
+    }, activateEvents() {
+        var classEventListener = document.getElementsByClassName("event-listener");
+
+        for(var i = 0; i < classEventListener.length; i++) {
+            classEventListener[i].addEventListener("click", function(event) {
+                console.log(event);
+                console.log(this);
+                this.classList.add("focus-styling");
+            })
+        }
+
+
     }
   };
 
--- js/quiz.js
+++ js/quiz.js
@@ -7,13 +7,56 @@ Loop over your array of cars and build up an HTML string to
  entire string of columns/rows before injecting into the DOM.
  Use a counter variable to know when to close a row after three columns.*/
 
- function populatePage (inventory) {
+
+
+
+function createCard(car, i) {
+    // This function will be passed 1 car at a time
+    var card = "";
+
+    if(i === 0 || i === 3 || i === 6 || i === 9 || i === 12) {
+        card += `<div class="row">`;
+        console.log("Opening Div");
+    }
+    // create the card
+    card += `<div class="col-lg-4" id="card_${i}">`;
+        card += "<div class='black-border event-listener'>";
+            card += `<center><img src="${car.src}" width="80%" height="70%"></center>`; // add img to json file
+            card += `<h3><center><span>${car.make}</span></center></h3>`;
+            card += `<h3><center><span>${car.model}</span></center></h3>`;
+            card += `<h3><center><span>${car.year}</span></center></h3>`;
+            card += `<h3><center><span>$${car.price}</span></center></h3>`;
+            card += `<p>${car.description}</p>`
+        card += `</div>`;
+    card += `</div>`;
+
+    if(i == 2 || i === 5 || i === 8 || i === 11) {
+        card += `</div>`;
+        console.log("closing div");
+    }
+    // add to the dom
+    console.log(card);
+    document.getElementById("container").innerHTML += card;
+
+}
+
+function populatePage (inventory) {
+    console.log(inventory);
   // Loop over the inventory and populate the page
+    document.getElementById("container").innerHTML += "<center><h2>Useds Cars</h2></center>";
+    for(var i = 0; i < inventory.cars.length; i++) {
+        createCard(inventory.cars[i], i);
+    }
 
   // Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
+    CarLot.activateEvents(); // pass id to this function???
 }
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
-CarLot.loadInventory();
+console.log(CarLot);
+CarLot.loadInventory(populatePage);
+
+
+
+
